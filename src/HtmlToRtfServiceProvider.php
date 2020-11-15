45a4a6b5e14a567838e0c8cfd693c74f43f1f542
gedeeinstein <gedeeinstein@gmail.com>
2020-11-15 18:50:32 +0800

    Create HtmlToRtfServiceProvider.php

---
+++ src/HtmlToRtfServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Gedeeinstein\HtmlToRtf;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;;
+
+class WordTemplateServiceProvider extends BaseServiceProvider
+{
+    public function register()
+    {
+        $this->app->bind('word-template', function() {
+            return new HtmlToRtf;
+        });
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
